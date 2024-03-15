cafb79d9bd5b460919f4ce9eb81191086b10bba9
taipain <131139547+taipain@users.noreply.github.com>
2024-03-15 10:37:04 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,5 +1,5 @@
 from icrawler.builtin import GoogleImageCrawler
-import torch
+
 def download_images(keyword, max_num):
     google_crawler = GoogleImageCrawler(storage={"root_dir": keyword})
     google_crawler.crawl(keyword=keyword, max_num=max_num)
